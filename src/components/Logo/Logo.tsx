ab329509e34e645be4f071c92fac3c01f61559e7
Kasia <kajzerkatarzyna1@gmail.com>
2023-02-14 15:14:20 +0100

    Added new component Logo with Link to home page.

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Logo.css';
+import {Link} from "react-router-dom";
+
+interface Props {
+    text: string;
+    to?: string;
+}
+
+export const Logo = (props: Props) => (
+    props.to
+        ? <Link className="logo" to={props.to}>{props.text}</Link>
+        : <h2>{props.text}</h2>
+);
